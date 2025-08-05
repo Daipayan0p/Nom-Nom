1db01b3be63b3ff39e3ae93d5674ce1da5b76751
Daipayan0p <d.neogi.coding@gmail.com>
2025-08-05 19:53:33 +0530

    init

---
+++ backend/RandomStuff/a.py
@@ -0,0 +1,18 @@
+class DailyMeals:
+    def __init__(self, breakfast, lunch, dinner,snacks):
+        self.breakfast = breakfast
+        self.lunch = lunch
+        self.snacks = snacks
+        self.dinner = dinner
+
+
+
+
+day1 = DailyMeals(breakfast=["Gobi Paratha, Curd,","Pongal, Chutney, Sambar","B,B,J  Tea, Coffee, Milk,"],
+                  lunch=["Phulka, Dal Masala, White Rice,Sambar","Rasam,Curd, Onion Rings","Green Veg Sabzhi","Sweet: Badushai/Coconut Laddu "],
+                  snacks=["Sweet Corn","Tea,Coffee,Milk"],
+                  dinner=["Poori,White Rice ","Rasam, Sambar, Loose Curd ","Jeera Rice, Poriyal","Fruits : Banana"]
+                  )
+
+day15 = day1
+
